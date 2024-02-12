12b08a286b3fc48fd8f9bf84b9c68885817b57b7
wengmaxime <wengmaxime@outlook.fr>
2024-02-12 21:24:16 +0100

    insert value into table for test purpose

--- init.sql
+++ init.sql
@@ -29,3 +29,15 @@ CREATE TABLE reservation(
     CONSTRAINT proprietaire FOREIGN KEY(id) REFERENCES utilisateur(id), 
     CONSTRAINT lieu FOREIGN KEY(id) REFERENCES appartement(id)
 );
+
+-- 1 user pr statut
+-- 1 appart propriétaire
+-- tout à true
+
+INSERT into utilisateur (nom, mdp, status) VALUES ("user1", "password",1);
+INSERT into utilisateur (nom, mdp, status) VALUES ("user2", "password",2);
+INSERT into utilisateur (nom, mdp, status) VALUES ("user3", "password",3);
+
+INSERT into addresse (lieu) VALUES ("Mars");
+
+INSERT into appartement (capacite, superficie, disponible, prix, valide_admin, valide_proprio, proprietaire,addresse) VALUES (2,80,1,12000,1,1,2,1);
