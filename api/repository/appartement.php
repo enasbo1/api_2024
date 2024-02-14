88e94aec3f0f02c2d278d39b853bdaf1d4f588a7
enasbo1 <irenee.du.gardin@gmail.com>
2024-02-14 23:24:00 +0100

    correctif code appartement

--- api/repository/appartement.php
+++ api/repository/appartement.php
@@ -5,38 +5,36 @@ include_once("./shared/appartement.php");
 
 class Repository_appartement extends Repository_origin
 {
-	private $connection;
-
-    public function __construct() {
-        try {
-            $this->connection = pg_connect("host=database port=5432 dbname=todo_db user=todo password=password");
-            if ($this->connection == null) {
-                throw new Exception("Could not connect to database.");
-            }
-        } catch (Exception $e) {
-            throw new Exception("Database connection failed :" . $e->getMessage());
-        }
-    }
-
-    public function get_all() {
-        $query = "SELECT * FROM appartements";
-        $result = pg_query($this->connection, $query);
-        return pg_fetch_all($result);
-    }
-
-    public function get_by_id($id) {
-        $query = "SELECT * FROM appartements WHERE id = $1";
-        $result = pg_query_params($this->connection, $query, array($id));
-        return pg_fetch_assoc($result);
-    }
-
-    public function add_new($data) {
-        $query = "INSERT INTO appartements (id, addresse, capacite, superficie, disponible, prix, propriétaire) VALUES ($id, $data["addresse"], $data["capacite"], $data["superficie"], $data["disponible"], $data["prix"], $user_id)";
-        $result = pg_query_params($this->connection, $query, array_values($data));
+    public function get_all()
+    {
+        $result = $this->get('appartement', [
+            'id',
+            'capacite',
+            'superficie',
+            'disponible',
+            'prix',
+            'valide_admin',
+            'valide_proprio',
+            'proprietaire',
+            'addresse'
+        ], []);
         return $result;
     }
 
-    public function update($id, $data) {
+    public function get_by_id($id)
+    {
+        $query = "SELECT * FROM appartement WHERE id = $1";
+        $result = $this->query_params($query, array($id));
+        return $result;
+    }
+
+    public function add_new($data)
+    {
+        $this->post('appartement', $data);
+    }
+
+    public function update_one($id, $data)
+    {
         $setPart = [];
         $values = [];
         foreach ($data as $key => $value) {
@@ -44,16 +42,17 @@ class Repository_appartement extends Repository_origin
             $values[] = $value;
         }
         $setPartString = implode(', ', $setPart);
-        $query = "UPDATE appartements SET $setPartString WHERE id = ?";
+        $query = "UPDATE appartement SET $setPartString WHERE id = ?";
         $values[] = $id;
 
-        $result = pg_query_params($this->connection, $query, $values);
+        $result = $this->query_params($query, $values);
         return $result;
     }
 
-    public function delete($id) {
-        $query = "DELETE FROM appartements WHERE id = $1";
-        $result = pg_query_params($this->connection, $query, array($id));
+    public function delete_one($id)
+    {
+        $query = "DELETE FROM appartement WHERE id = $1";
+        $result = $this->query_params($query, array($id));
         return $result;
     }
 }
--- api/service/appartement.php
+++ api/service/appartement.php
@@ -4,31 +4,35 @@ include_once("./shared/appartement.php");
 
 class Service_appartement
 {
-    private Repository_appartement $repostitory;
+    private Repository_appartement $repository;
 
     public function __construct()
     {
-        $this->repostitory = new Repository_appartement;
+        $this->repository = new Repository_appartement;
     }
 
-    public function get_list() {
-            return $this->repository->get_all();
-        }
+    public function get_list()
+    {
+        return $this->repository->get_all();
+    }
 
-        public function get_appartement($id) {
-            return $this->repository->get_by_id($id);
-        }
+    public function get_appartement($id)
+    {
+        return $this->repository->get_by_id($id);
+    }
 
-        public function create_appartement($data) {
-            return $this->repository->add_new($data);
-        }
+    public function create_appartement($data)
+    {
+        $this->repository->add_new($data);
+    }
 
-        public function update_appartement($id, $data) {
-            return $this->repository->update($id, $data);
-        }
+    public function update_appartement($id, $data)
+    {
+        return $this->repository->update_one($id, $data);
+    }
 
-        public function delete_appartement($id) {
-            return $this->repository->delete($id);
-        }
+    public function delete_appartement($id)
+    {
+        return $this->repository->delete_one($id);
     }
 }
