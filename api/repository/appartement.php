d9f47e98d8e430ed0ab6e214c3fdbe319be2d0ab
Nemesort <thibaultgouchon@gmail.com>
2024-02-15 02:47:51 +0100

    Mise à jour appartement

--- api/repository/appartement.php
+++ api/repository/appartement.php
@@ -2,58 +2,70 @@
 include_once("./repository/origin.php");
 include_once("./shared/appartement.php");
 
-
 class Repository_appartement extends Repository_origin
 {
-	private $connection;
+    private $connection;
+    private string $table = "appartement";
 
-    public function __construct() {
-        try {
-            $this->connection = pg_connect("host=database port=5432 dbname=todo_db user=todo password=password");
-            if ($this->connection == null) {
-                throw new Exception("Could not connect to database.");
+    public function get_appartement($id) {
+        if ($id != null) {
+            $query = "SELECT * FROM {$this->table} WHERE id = $1";
+            $result = pg_query_params($this->connection, $query, [$id]);
+            return pg_fetch_assoc($result);
+        } else {
+            $query = "SELECT * FROM {$this->table}";
+            $result = pg_query($this->connection, $query);
+            return pg_fetch_all($result);
+        }
+    }
+
+    public function add_new(Appartement $appartement) {
+        $query = "INSERT INTO {$this->table} (capacite, superficie, disponible, prix, id_proprio, id_addresse) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+        $params = [
+            $appartement->capacite, 
+            $appartement->superficie, 
+            $appartement->disponible, 
+            $appartement->prix, 
+            $appartement->id_proprio, 
+            $appartement->id_addresse
+        ];
+        pg_query_params($this->connection, $query, $params);
+    }
+    
+
+    public function update_appartement(Appartement $appartement) {
+        $setParts = [];
+        $params = [];
+        $i = 1;
+
+        $fieldsToUpdate = [
+            'capacite' => $appartement->capacite,
+            'superficie' => $appartement->superficie,
+            'disponible' => $appartement->disponible,
+            'prix' => $appartement->prix,
+            'v_admin' => $appartement->v_admin,
+            'v_proprio' => $appartement->v_proprio,
+            'id_proprio' => $appartement->id_proprio,
+            'id_addresse' => $appartement->id_addresse,
+        ];
+    
+        foreach ($fieldsToUpdate as $field => $value) {
+            if (isset($value)) {
+                $setParts[] = "$field = \$$i";
+                $params[] = $value;
+                $i++;
             }
-        } catch (Exception $e) {
-            throw new Exception("Database connection failed :" . $e->getMessage());
         }
-    }
+    
+        $params[] = $appartement->id;
+        $setString = implode(', ', $setParts);
+        $query = "UPDATE {$this->table} SET $setString WHERE id = \$$i";
+    
+        pg_query_params($this->connection, $query, $params);
+    }    
 
-    public function get_all() {
-        $query = "SELECT * FROM appartements";
-        $result = pg_query($this->connection, $query);
-        return pg_fetch_all($result);
-    }
-
-    public function get_by_id($id) {
-        $query = "SELECT * FROM appartements WHERE id = $1";
-        $result = pg_query_params($this->connection, $query, array($id));
-        return pg_fetch_assoc($result);
-    }
-
-    public function add_new($data) {
-        $query = "INSERT INTO appartements (id, addresse, capacite, superficie, disponible, prix, propriétaire) VALUES ($id, $data["addresse"], $data["capacite"], $data["superficie"], $data["disponible"], $data["prix"], $user_id)";
-        $result = pg_query_params($this->connection, $query, array_values($data));
-        return $result;
-    }
-
-    public function update($id, $data) {
-        $setPart = [];
-        $values = [];
-        foreach ($data as $key => $value) {
-            $setPart[] = "$key = ?";
-            $values[] = $value;
-        }
-        $setPartString = implode(', ', $setPart);
-        $query = "UPDATE appartements SET $setPartString WHERE id = ?";
-        $values[] = $id;
-
-        $result = pg_query_params($this->connection, $query, $values);
-        return $result;
-    }
-
-    public function delete($id) {
-        $query = "DELETE FROM appartements WHERE id = $1";
-        $result = pg_query_params($this->connection, $query, array($id));
-        return $result;
+    public function delete_appartement($id) {
+        $query = "DELETE FROM {$this->table} WHERE id = $1";
+        pg_query_params($this->connection, $query, [$id]);
     }
 }
--- api/service/appartement.php
+++ api/service/appartement.php
@@ -1,34 +1,52 @@
 <?php
 include_once("./repository/appartement.php");
-include_once("./shared/appartement.php");
 
 class Service_appartement
 {
-    private Repository_appartement $repostitory;
+    private Repository_appartement $repository;
 
     public function __construct()
     {
-        $this->repostitory = new Repository_appartement;
+        $this->repository = new Repository_appartement();
     }
 
-    public function get_list() {
-            return $this->repository->get_all();
+    public function get_appartement($id) {
+        try {
+            $result = $this->repository->get_appartement($id);
+            if ($result) {
+                resolve_with_content(200, $result);
+            } else {
+                resolve_with_message(404, "Appartement not found");
+            }
+        } catch (Exception $e) {
+            resolve_with_message(500, "An error occurred: " . $e->getMessage());
         }
+    }
 
-        public function get_appartement($id) {
-            return $this->repository->get_by_id($id);
+    public function add_new(Appartement $appartement) {
+        try {
+            $this->repository->add_new($appartement);
+            resolve_with_message(201, "Appartement added successfully");
+        } catch (Exception $e) {
+            resolve_with_message(500, "An error occurred: " . $e->getMessage());
         }
+    }
 
-        public function create_appartement($data) {
-            return $this->repository->add_new($data);
+    public function update_appartement(Appartement $appartement) {
+        try {
+            $this->repository->update_appartement($appartement);
+            resolve_with_message(204, "Appartement updated successfully");
+        } catch (Exception $e) {
+            resolve_with_message(500, "An error occurred: " . $e->getMessage());
         }
+    }
 
-        public function update_appartement($id, $data) {
-            return $this->repository->update($id, $data);
-        }
-
-        public function delete_appartement($id) {
-            return $this->repository->delete($id);
+    public function delete_appartement($id) {
+        try {
+            $this->repository->delete_appartement($id);
+            resolve_with_message(200, "Appartement deleted successfully");
+        } catch (Exception $e) {
+            resolve_with_message(500, "An error occurred: " . $e->getMessage());
         }
     }
 }
